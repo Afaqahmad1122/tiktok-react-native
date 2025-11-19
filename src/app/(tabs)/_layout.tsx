dba5534f78e5c391f29bdb2ead5806e69793e7e9
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2025-11-19 15:27:27 +0500

    Add tab navigation and update app configuration
    - Introduced tab navigation with screens for Home, Friends, New Post, Inbox, and Profile.
    - Updated app.json to include a scheme and added expo-router as a plugin.
    - Changed the main entry point in package.json to use expo-router.
    - Removed the old App.tsx and index.ts files.
    - Added TypeScript path mappings in tsconfig.json for better module resolution.
    - Updated package-lock.json and package.json with new dependencies for navigation and icons.

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,57 @@
+import { Tabs } from "expo-router";
+import Entypo from "@expo/vector-icons/Entypo";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+export default function TabsLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="friends"
+        options={{
+          title: "Friends",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="users" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="newPost"
+        options={{
+          title: "New Post",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="plus" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          title: "Inbox",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="envelope" color={color} size={size} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="user" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ src/app/(tabs)/newPost.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const NewPostScereen = () => {
+  return (
+    <View>
+      <Text>NewPostScereen</Text>
+    </View>
+  );
+};
+
+export default NewPostScereen;
